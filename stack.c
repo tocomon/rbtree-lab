9a16c00c4f1da03c6be7ce8025ba8c39362044cf
tocomon <128657695+tocomon@users.noreply.github.com>
2023-05-05 10:37:04 +0900

    stack

--- stack.c
+++ stack.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #define MAX_STACK_SIZE 100
- 
+
 int stack[MAX_STACK_SIZE];
-int top=-1;
- 
+int top = -1;
+
 int IsEmpty(){
     if(top<0)
         return 1;
     else
         return 0;
     }
-    
+
 int IsFull(){
     if(top>=MAX_STACK_SIZE-1)
         return 1;
@@ -20,23 +20,31 @@ int IsFull(){
  
 void push(int value){
     if(IsFull()==1)
-        printf("스택이 가득 찼습니다.");
+        printf("stack is full\n");
     else
-        stack[++top]=value; 
+        stack[++top] = value; 
 }
  
 int pop(){
     if(IsEmpty()==1)
-        printf("스택이 비었습니다.");
+        printf("stack is empty\n");
     else 
         return stack[top--];
 }
+
+void printStack(){
+int i;
+   for(i=0; i<=top; i++){
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
  
 int main(){
-    
     push(3);
     push(5);
     push(12);
+    printStack();
     printf("%d ",pop());
     printf("%d ",pop());
     printf("%d ",pop());
